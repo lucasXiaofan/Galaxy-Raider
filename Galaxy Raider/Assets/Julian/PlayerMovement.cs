b44ea391547bcb79edef541055fd5ec2db3c5546
vaperyy <jingjl23@uci.edu>
2022-05-01 01:12:24 -0700

    fixed slight player bug
    With character controller and height.

--- Galaxy Raider/Assets/Julian/PlayerMovement.cs
+++ Galaxy Raider/Assets/Julian/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.NetworkInformation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -17,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     Vector3 velocity;
+    bool isGrounded;
+
     void Start()
     {
         
@@ -25,12 +30,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
