77097376932f8bbdc6c24e3466477669f5428427
lucasXiaoFan <xiaofl14@uci.edu>
2022-04-30 17:39:36 -0700

    add_stuff
    1. script
    2. enmey
    3. hookshot stuff

---
+++ Galaxy Raider/Assets/BossFightRelated/Weapon_test_code.cs
@@ -0,0 +1,62 @@
+
+using UnityEngine;
+
+public class Weapon_test_code : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float range = 50f;
+    [SerializeField] float damage = 30f;
+    [SerializeField] GameObject hiteffect;
+    public float bullet_live_time = 3.0f;
+    public bool canShoot = true;
+    public float fireRate = 15f;
+    private float nextTimeToShoot = 0f;
+
+
+
+
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= nextTimeToShoot)
+        {
+            nextTimeToShoot = Time.time + 1f / fireRate;
+            ShootAuto();
+        }
+
+    }
+
+
+    private void ShootAuto()
+    {
+        processRayCast();
+    }
+
+    private void processRayCast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
+        {
+            PlayhitEffect(hit);
+            Debug.Log(hit.transform.name);
+        }
+        else
+        {
+            return;
+        }
+
+    }
+
+    private void PlayhitEffect(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hiteffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, bullet_live_time);
+
+    }
+}
